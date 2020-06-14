52af8aee698e5a4c6d5c901bf87d69bddc9e0a02
SlawomirKalisztan <65415285+SlawomirK77@users.noreply.github.com>
2020-06-14 13:22:09 +0200

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -5,8 +5,8 @@ import assets
 
 
 class KomorkaTest(unittest.TestCase):
-    def przygotuj(self):
-        self.komorka = main.Komorka(kolumna=1, wiersz=1, bomba=False, wybrana=False, flaga_bomba=False,
+    def setUp(self):
+        self.komorka = main.Komorka(kolumna=5, wiersz=4, bomba=False, wybrana=False, flaga_bomba=False,
                                      flaga_bomba_moze=False)
 
     def test_bomba(self):
@@ -20,29 +20,6 @@ class KomorkaTest(unittest.TestCase):
 
     def test_flaga_bomba_moze(self):
         self.assertFalse(self.komorka.flaga_bomba_moze)
-
-    def test_identyfikuj_miny(self):
-        plansza = []
-        self.assertEqual(self.komorka.identyfikuj_miny(plansza, 0, 0), 0)
-
-
-'''
-    def test_plansza(self):
-        ile_wierszy = 3
-        ile_kolumn = 4
-        plansza = []
-        main.inicjuj_pusta_plansze(plansza, ile_kolumn, ile_wierszy)
-        for i in [0, 4, 7]:
-            plansza[i].bomba=True
-
-        przewidywania = [True, False, False, False,
-                         True, False, False, False,
-                         True, False, False, False]
-
-        #self.assertListEqual(plansza,przewidywania)
-        for i in range(ile_kolumn * ile_wierszy):
-            self.assertEqual(plansza[i], przewidywania[i])
-'''
 
 
 class TestAssets(unittest.TestCase):
